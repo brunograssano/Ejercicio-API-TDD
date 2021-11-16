5e0653d8888574f96025468f609cd6586163550c
Diego Montaldo <diego@Diegos-MBP-2.fibertel.com.ar>
2021-11-16 16:36:10 -0300

    Added dotenv library and SERVER PORT was moved to .env file
    Includes dependency to dotenv
    Add SERVER_PORT_API in the .env file

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
 
-//import * as dotenv from "dotenv";
-//dotenv.config({ path: __dirname+'/.env' });
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
+
 import { Router } from 'express';
 import usersRoutes from './src/routes/usersRoutes';
 import utilsRoutes from './src/routes/utilsRoutes';
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.SERVER_API_PORT;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
