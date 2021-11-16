571877835f1f77de5be128357e2ec059e2c99eaf
Diego Montaldo <diego@Diegos-MBP-2.fibertel.com.ar>
2021-11-16 16:44:17 -0300

    Added SERVER_DB_HOST to .env file
    Added string connection to configuration file

--- index.js
+++ index.js
@@ -5,20 +5,19 @@ import bodyParser from 'body-parser';
 
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname+'/.env' });
+const PORT = process.env.SERVER_API_PORT;
+const SERVER_DB_HOST = process.env.SERVER_DB_HOST;
+
 
 import { Router } from 'express';
 import usersRoutes from './src/routes/usersRoutes';
 import utilsRoutes from './src/routes/utilsRoutes';
 
 const app = express();
-const PORT = process.env.SERVER_API_PORT;
 
 // mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/rest-exercise-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(SERVER_DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true} );
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
