f8300238eca342cad35a6441f57dabb7a0c8ed5c
brunograssano <grassanobruno@gmail.com>
2021-12-02 12:27:44 -0300

    test: Added test of getUserWithID

--- tests/userService.test.ts
+++ tests/userService.test.ts
@@ -1,15 +1,20 @@
 import chai = require('chai');
 import chaiHttp = require('chai-http');
+import {decodeSession} from "../src/services/sessionService";
+import {DecodeResult, Session} from "../src/models/sessionModel";
 const expect = chai.expect
 const baseUrl = "http://localhost:4000"
 chai.use(chaiHttp);
 
+const SECRET_KEY_TEST : string = "akljsdfbuw3 c;keh20342 f394nm0mc@!#!$%!F$^ e"
+
 describe("User Service Tests", () => {
-    let token;
+    let token : string;
+    let userSession : Session;
     it('Should return successful sign up', (done) => {
         chai.request(baseUrl)
             .post('/users')
-            .auth('user','pass', {type:"basic"})
+            .auth('test','test', {type:"basic"})
             .send({'email':'test-email@test.com',
                     'firstName':'test',
                     'lastName':'test'})
@@ -20,4 +25,29 @@ describe("User Service Tests", () => {
                 done();
             });
     });
+
+    it('Should return the information about the new user', (done) => {
+        const decodedSession: DecodeResult = decodeSession(SECRET_KEY_TEST, token);
+        if (decodedSession.type === 'valid'){
+            userSession = decodedSession.session;
+        } else{
+            return;
+        }
+        chai.request(baseUrl)
+            .get('/manage/users/' + userSession.id)
+            .set('JWT-Token',token)
+            .end((error , response) => {
+                expect(response.status).equal(200);
+                console.log(response.body)
+                expect(response.body.email).equal('test-email@test.com');
+                expect(response.body.firstName).equal('test');
+                expect(response.body.lastName).equal('test');
+                expect(response.body.username).equal('test');
+                expect(response.body._id).equal(userSession.id);
+                done();
+            });
+    });
+
+
+
 })
